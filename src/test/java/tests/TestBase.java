4c926e32d7d73436156aad218f93f4bdf4eb138a
Ekaterina Goncharova <katinagon@yandex.ru>
2025-05-29 16:33:38 +0300

    Added allure, tasks

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -1,13 +1,53 @@
 package tests;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import helpers.Attach;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.util.Map;
+import java.util.UUID;
 
 public class TestBase {
+    private static final String SELENOID_URL = System.getProperty("selenoid.url");
+    private static final String SELENOID_LOGIN = System.getProperty("selenoid.login");;
+    private static final String SELENOID_PASSWORD = System.getProperty("selenoid.password");
+
     @BeforeAll
     public static void beforeAll() {
+        Configuration.browser = System.getProperty("browser", "chrome");
+        Configuration.browserVersion = System.getProperty("browser.version", "128.0");
+        Configuration.browserSize = System.getProperty("browser.size", "1920x1080");
         Configuration.baseUrl = "https://crowdtesting.ru/";
-        Configuration.browserSize = "1920x1080";
         Configuration.pageLoadStrategy = "eager";
+    }
+
+    @BeforeEach
+    public void beforeEach() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
+                "enableVNC", true,
+                "enableVideo", true,
+                "name", "Test: " + UUID.randomUUID()
+        ));
+        Configuration.remote = "https://" + SELENOID_LOGIN + ":" + SELENOID_PASSWORD + "@" + SELENOID_URL + "/wd/hub";
+        Configuration.browserCapabilities = capabilities;
+        Configuration.holdBrowserOpen = false;
+    }
+
+    @AfterEach
+    public void afterEach() {
+        Attach.screenshotAs("Скриншот результата");
+        Attach.pageSource();
+        if (!Configuration.browser.equals("firefox"))
+            Attach.browserConsoleLogs();
+        Attach.addVideo();
+        Selenide.closeWebDriver();
     }
 }
